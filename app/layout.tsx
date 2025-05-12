a5a892d89b6637e757ad4db141aff2e5fc5c60cc
Ayrus707 <122613157+Ayrus707@users.noreply.github.com>
2025-05-12 20:09:29 +0530

    downgraded to nexjs 14

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import ToasterContext from "./context/ToasterContext";
 import AuthContext from "./context/AuthContext";
 import ActiveStatus from "./components/ActiveStatus";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+// const geistSans = Geist({
+//   variable: "--font-geist-sans",
+//   subsets: ["latin"],
+// });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+// const geistMono = Geist_Mono({
+//   variable: "--font-geist-mono",
+//   subsets: ["latin"],
+// });
+
+
+
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -28,7 +36,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${inter.variable} antialiased`}
       >
         <AuthContext>
         <ToasterContext/>
---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   images: {
+//     domains: ["res.cloudinary.com","avatars.githubusercontent.com","lh3.googleusercontent.com"],
+//   },
+// };
+
+// export default nextConfig;
+// next.config.mjs (pure JS — no types needed)
+
+const nextConfig = {
+  images: {
+    domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"],
+  },
+};
+
+export default nextConfig;
